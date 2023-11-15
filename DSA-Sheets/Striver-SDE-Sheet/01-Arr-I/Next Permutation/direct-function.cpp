0d52e5098e7db29147a88e3df780707ff4e56698
Sumit Jha <146546239+zhasumit@users.noreply.github.com>
2023-11-15 17:53:53 +0000

    next permutation

---
+++ DSA-Sheets/Striver-SDE-Sheet/01-Arr-I/Next Permutation/direct-function.cpp
@@ -0,0 +1,13 @@
+// n numbers => n factorial solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
